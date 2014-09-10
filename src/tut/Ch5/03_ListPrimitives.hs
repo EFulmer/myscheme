db8926b5f525a15cc4302393ead5c8ac6bd702f5
Eric Fulmer <EFulmer@gmail.com>
2014-09-09 20:10:00 -0400

    fixed issue with eqv

--- src/tut/Ch5/03_ListPrimitives.hs
+++ src/tut/Ch5/03_ListPrimitives.hs
@@ -453,7 +453,7 @@ car [badArg]              = throwError $ TypeMismatch "pair" badArg
 car badArgList            = throwError $ NumArgs 1 badArgList
 
 
-cdr :: [LispVal] -> TypeMismatch LispVal
+cdr :: [LispVal] -> ThrowsError LispVal
 cdr [List (x:xs)]         = return $ List xs
 cdr [DottedList [_] x]    = return x
 cdr [DottedList (_:xs) x] = return $ DottedList xs x
@@ -470,16 +470,19 @@ cons badArgList            = throwError $ NumArgs 2 badArgList
 
 
 eqv :: [LispVal] -> ThrowsError LispVal
-eqv [(Bool arg1), (Bool arg2)] = return $ Bool $ arg1 == arg2
-eqv [(Number arg1, Number arg2)] = return $ Bool $ arg1 == arg2
-eqv [(String arg1, String arg2)] = return $ Bool $ arg1 == arg2
-eqv [(DottedList xs x), (DottedList ys y)] = return $ Bool $ (length arg1 == length arg2) && 
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List $ xs ++ [x], List $ ys ++ [y]]
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) && 
                                                              (all eqvPair $ zip arg1 arg2)
-                                             where eqvPair (x1, x2) = case eqv [x1, x2] of
-                                                Left err -> False
-                                                Right (Bool val) -> val
 eqv [_, _]                                 = return $ Bool False
 eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
+eqvPair (x1, x2) = case eqv [x1, x2] of
+    Left err -> False
+    Right (Bool val) -> val
 
 -- End primitive definitions
 -- Begin error handling definitions
